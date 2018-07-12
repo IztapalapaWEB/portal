7e8057e3675c14653d1756a1d17c3a022a03b9a3
IztapalapaWEB <38738006+IztapalapaWEB@users.noreply.github.com>
2018-07-12 10:24:49 -0500

    index.php
    créate new file index

---
+++ index.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Documento sin título</title>
+<link href="file:///C|/wamp64/www/web/bootstrap/css/bootstrap.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+ Hola Mundo Virtual
+</body>
+</html>
